e8c12608dafdd5b9f0c9f04c3066b3f995ebef13
Nandhini <nandhinimanivannan1523@gmail.com>
2026-05-30 19:19:05 +0530

    feat: add login page object

---
+++ src/main/java/com/parabank/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.parabank.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private final WebDriver driver;
+
+    private static final By USERNAME =
+            By.name("username");
+
+    private static final By PASSWORD =
+            By.name("password");
+
+    private static final By LOGIN_BUTTON =
+            By.cssSelector("input[value='Log In']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String username,
+                      String password) {
+
+        driver.findElement(USERNAME)
+                .sendKeys(username);
+
+        driver.findElement(PASSWORD)
+                .sendKeys(password);
+
+        driver.findElement(LOGIN_BUTTON)
+                .click();
+    }
+}
